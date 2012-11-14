fb108bb01275011d85e2bb136b11cf3c507c4abc
Miles Steele <miles@milessteele.com>
2012-11-14 16:37:58 -0500

    init

---
+++ src/sketch.ino
@@ -0,0 +1,12 @@
+const int led_pin = 13;
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led_pin, HIGH);
+  delay(100);
+  digitalWrite(led_pin, LOW);
+  delay(900);
+}
