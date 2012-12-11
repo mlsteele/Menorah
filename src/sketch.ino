9482a9ff97c31d2fbcdec0f1196f2b92ac70e2c5
Miles Steele <miles@milessteele.com>
2012-12-11 00:36:03 -0500

    flicker

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,6 +1,8 @@
 const int candle_pins_len = 8;
 int candle_pins[8] = {6, 12, 4, 5, 11, 9, 10, 7};
 float candle_lums[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+// int candles_time_on[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+// int candles_time_last_t = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -8,26 +10,44 @@ void setup() {
   for (int i = 0; i < candle_pins_len; i++) {
     pinMode(candle_pins[i], OUTPUT);
     candle_lums[i] = frand();
+    // candle_lums[i] = 1;
   }
 
   print_lums();
+  // candles_time_last_t = millis();
 }
 
 void loop() {
-  update_candles();
+  draw_candles();
   fade_candles();
+  delay(10);
+  // time_count_candles();
 }
 
-void update_candles() {
+void draw_candles() {
   for (int i = 0; i < candle_pins_len; i++) {
     digitalWrite(candle_pins[i], lum_map(frand()) < candle_lums[i]);
+
     // analogWrite(candle_pins[i], candle_lums[i] * 128);
+
+    // if (candles_time_on[i] > 1000 * candle_lums[i]) {
+    //   candles_time_on[i] = 0;
+    //   digitalWrite(candle_pins[i], !digitalRead(candle_pins[i]));
+    // }
   }
 }
 
+// void time_count_candles() {
+//   int step = millis() - candles_time_last_t;
+//   candles_time_last_t = millis();
+//   for (int i = 0; i < candle_pins_len; i++) {
+//     candles_time_on[i] += step;
+//   }
+// }
+
 void fade_candles() {
   for (int i = 0; i < candle_pins_len; i++) {
-    candle_lums[i] -= 0.00001;
+    candle_lums[i] -= 0.0001;
     candle_lums[i] = fmax(0, candle_lums[i]);
   }
 }
